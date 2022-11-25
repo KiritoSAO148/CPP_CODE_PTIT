9d3eec195df12b41288d012f39aa80da0f68802e
B20DCPT094 <wuochuy02@gmail.com>
2022-11-26 06:57:19 +0700

    CPP

--- CPP0205-DAYTAMGIAC.cpp
+++ CPP0205-DAYTAMGIAC.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	int t;
 	cin >> t;
 	while (t--){
@@ -11,8 +12,7 @@ int main(){
 		for(int i=0; i<n; i++){
 			cin >> a[i];
 		}
-		sort (a,a+n,greater<int>());
-		cout << a[0] << endl;
+		cout << *max_element(a, a + n) << '\n';
 	}
 	return 0;
 }
