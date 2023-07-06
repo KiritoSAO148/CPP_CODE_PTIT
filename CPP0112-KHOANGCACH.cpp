1d9923aee07a9cc85b0cdd07120a4bccdfb7847f
B20DCPT094 <wuochuy02@gmail.com>
2023-07-06 12:04:59 +0700

    CPP

--- CPP0112-KHOANGCACH.cpp
+++ CPP0112-KHOANGCACH.cpp
@@ -1,16 +1,46 @@
-#include <iostream>
-#include <math.h>
-#include <iomanip>
+#include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define ms(s,n) memset(s,n,sizeof(s))
+#define all(a) a.begin(),a.end()
+#define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define maxn 
+
+const int MOD = (int) 1e9+7;
+
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
 int main(){
-	int t;
-	double k;
-	cin >> t;
-	while (t--){
-		double a,b,c,d;
-		cin >> a >> b >> c >> d;
-		k=sqrt((a-c)*(a-c)+(b-d)*(b-d));
-		cout << fixed << setprecision(4) << k << endl;
-	}
+    FileIO();
+    FastIO;
+    TC(){
+        double x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        cout << fixed << setprecision(4) << sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); el;
+    }
+    return 0;
 }
--- CPP108-SOTANGGIAM.cpp
+++ CPP108-SOTANGGIAM.cpp
@@ -1,72 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
 #define ms(s,n) memset(s,n,sizeof(s))
 #define all(a) a.begin(),a.end()
 #define sz(a) int((a).size())
 #define f0(i,n) for (int i=0; i<n; i++)
 #define f1(i,n) for (int i=1; i<=n; i++)
-#define FastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
 #define pb push_back
 #define pf push_front
 #define fi first
 #define se second
-#define maxn
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pi;
-typedef vector<int> vi;
-typedef vector<pi> vii;
+#define maxn 
 
 const int MOD = (int) 1e9+7;
 
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
 int prime (ll n){
-	if (n < 2) return 0;
-	for (int i=2; i<=sqrt(n); i++){
-		if (n%i==0) return 0;
-	}
-	return 1;
+    if (n < 2) return 0;
+    for (int i = 2; i <= sqrt(n); ++i){
+        if (n % i == 0) return 0;
+    }
+    return 1;
 }
 
-int up (ll n){
-	int x=n%10;
-	n/=10;
-	while (n){
-		int y=n%10;
-		if (y>=x) return 0;
-		x=y;
-		n/=10;
-	}
-	return 1;
+bool up(ll n){
+    int x = n % 10;
+    n /= 10;
+    while (n){
+        int y = n % 10;
+        if (y >= x) return false;
+        x = y;
+        n /= 10;
+    }
+    return true;
 }
 
-int down (ll n){
-	int x=n%10;
-	n/=10;
-	while (n){
-		int y=n%10;
-		if (y<=x) return 0;
-		x=y;
-		n/=10;
-	}
-	return 1;
+bool down(ll n){
+    int x = n % 10;
+    n /= 10;
+    while (n){
+        int y = n % 10;
+        if (y <= x) return false;
+        x = y;
+        n /= 10;
+    }
+    return true;
 }
 
 int main(){
-	#ifndef ONLINE_JUDGE
-  	freopen("input.txt", "r", stdin);
-  	freopen("output.txt", "w", stdout);
-  	#endif
-	FastIO;
-	int t; cin >> t;
-	while (t--){
-		int n; cin >> n;
-		int cnt=0;
-		for (int i=pow(10,n-1); i<pow(10,n); i++){
-			if ((up(i) || down(i)) && prime(i)) ++cnt;
-		}
-		cout << cnt << "\n";
-	}
-	return 0;
+    FileIO();
+    FastIO;
+    TC(){
+        int n; cin >> n;
+        int cnt = 0;
+        for (int i = pow(10, n - 1); i < pow(10, n); ++i){
+            if ((up(i) || down(i)) && prime(i)) ++cnt;
+        }
+        cout << cnt; el;
+    }
+    return 0;
 }
