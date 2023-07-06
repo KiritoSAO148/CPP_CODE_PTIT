2c7ae5b04bfc2acb32f80192434df2d867b7d964
B20DCPT094 <wuochuy02@gmail.com>
2023-07-06 12:26:53 +0700

    CPP

--- CPP0173-CHIAHETCHOBASONGUYEN.cpp
+++ CPP0173-CHIAHETCHOBASONGUYEN.cpp
@@ -1,52 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
 #define ms(s,n) memset(s,n,sizeof(s))
 #define all(a) a.begin(),a.end()
 #define sz(a) int((a).size())
 #define f0(i,n) for (int i=0; i<n; i++)
 #define f1(i,n) for (int i=1; i<=n; i++)
-#define FastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
 #define pb push_back
 #define pf push_front
 #define fi first
 #define se second
-#define maxn
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pi;
-typedef vector<int> vi;
-typedef vector<pi> vii;
+#define maxn 
 
 const int MOD = (int) 1e9+7;
 
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
 ll gcd (ll a, ll b){
-	if (b==0) return a;
-	return gcd(b,a%b);
+    if (b==0) return a;
+    return gcd(b,a%b);
 }
 
 ll lcm (ll a, ll b){
-	return a/gcd(a,b)*b;
+    return a/gcd(a,b)*b;
 }
 
-ll solve (int x, int y, int z, int n){
-	ll l = lcm(lcm(x,y),z);
-	ll res=((ll)pow(10,n-1)+l-1)/l*l;
-	if (res<(ll)pow(10,n)) return res;
-	return -1;
+ll solve(ll x, ll y, ll z, ll n){
+    ll res = lcm(lcm(x, y), z);
+    ll ans = ((ll)pow(10, n - 1) + res - 1) / res * res;
+    if (ans < pow(10, n)) return ans;
+    return -1;
 }
 
 int main(){
-	#ifndef ONLINE_JUDGE
-  	freopen("input.txt", "r", stdin);
-  	freopen("output.txt", "w", stdout);
-  	#endif
-	FastIO;
-	int t; cin >> t;
-	while (t--){
-		int x,y,z,n; cin >> x >> y >> z >> n;
-		cout << solve(x,y,z,n) << "\n";
-	}
-	return 0;
+    FileIO();
+    FastIO;
+    TC(){
+        ll x, y, z, n; cin >> x >> y >> z >> n;
+        cout << solve(x, y, z, n); el;
+    }
+    return 0;
 }
--- CPP0181-SOLAPLAI.cpp
+++ CPP0181-SOLAPLAI.cpp
@@ -1,17 +1,59 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define ms(s,n) memset(s,n,sizeof(s))
+#define all(a) a.begin(),a.end()
+#define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define maxn 
+
+const int MOD = (int) 1e9+7;
+
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+ll gcd (ll a, ll b){
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+ll lcm (ll a, ll b){
+    return a/gcd(a,b)*b;
+}
+
+void solve(ll a, ll x, ll y){
+    for (int i = 1; i <= gcd(x, y); ++i) cout << a;
+}
+
 int main(){
-	int t;
-	cin >> t;
-	while (t--){
-		long long a,x,y;
-		cin >> a >> x >> y;
-		long long s=__gcd(x,y);
-		for (int i=0; i<s; i++){
-			cout << a;
-		}
-		cout << endl;
-	}
+    FileIO();
+    FastIO;
+    TC(){
+        ll a, x, y; cin >> a >> x >> y;
+        solve(a, x, y); el;
+    }
+    return 0;
 }
