a7c3da58eb0f7992f98df90e309ed6d8dd4bfa43
B20DCPT094 <wuochuy02@gmail.com>
2023-07-26 17:43:46 +0700

    CPP

---
+++ CPP0361-MATKHAU.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define ms(s,n) memset(s,n,sizeof(s))
+#define all(a) a.begin(),a.end()
+#define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define maxn 
+
+const int MOD = (int) 1e9+7;
+
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+struct pass{
+    string p;
+    int len;
+};
+
+bool cmp(pass a, pass b){
+    if (a.len != b.len) return a.len > b.len;
+    return a.p < b.p;
+}
+
+int main(){
+    FileIO();
+    FastIO;
+    int n; cin >> n;
+    pass a[n];
+    f0 (i, n){
+        cin >> a[i].p;
+        a[i].len = sz(a[i].p);
+    }
+    sort(a, a + n, cmp);
+    int cnt = 0;
+    for (int i = 0; i < n - 1; ++i){
+        for (int j = i + 1; j < n; ++j){
+            if (a[i].p == a[j].p) cnt += 2;
+            else if (a[i].p.find(a[j].p) != string::npos) ++cnt;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
